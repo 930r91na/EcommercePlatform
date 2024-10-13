f4615dd60dc8a88d6d396ccf1b0b8310fdc42433
GEORGINAZERON <georgina.zeronca@udlap.mx>
2024-10-13 15:02:27 -0600

    more changes lol

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,11 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import LogIn from './components/LogIn.vue'
+import Home from "./components/Home.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
+
   }
 }
 </script>
